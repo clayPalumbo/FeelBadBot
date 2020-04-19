43a8b940429d6768b659ad147f15e5d8c3fe879d
Clay Palumbo <claypalumbo@clays-mbp.attlocal.net>
2020-04-18 23:09:28 -0400

    ethan addition

--- bot.js
+++ bot.js
@@ -36,7 +36,10 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     }
 
     let newMessage = message.toLowerCase();
-
+    if (user === "ethanp") {
+        const message = "Ethan is gay"
+        chatBot(message);
+    }
     if (newMessage.substring(0, 1) == '$') {
         let insult = insultGenerator();
 
